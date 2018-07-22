0dcba393977014d27b3431953846f63e5442ec6d
Olexiy Koval <kalex32@ukr.net>
2018-07-22 12:44:37 +0300

    ArrayRes try catch run

--- Koval_HW_26_05_18/src/ArrayRes/Run.java
+++ Koval_HW_26_05_18/src/ArrayRes/Run.java
@@ -10,13 +10,18 @@ public class Run {
 
         System.out.println("Введите значение элемента:");
         int value = scanner.nextInt();
-        System.out.println("Введите индекс элемента:");
-        int index = scanner.nextInt();
-        try {
-            if (index>arrayRes.getArrayR().length-1);
-        } catch ()
 
-        arrayRes.arrayInsert(value, index);
-        arrayRes.arrayPrint(arrayRes.getArrayR());
+            System.out.println("Введите индекс элемента:");
+            int index = scanner.nextInt();
+        try {
+            if (index>arrayRes.getArrayR().length-1){
+                arrayRes.arrayInsert(value, index);
+                arrayRes.arrayPrint(arrayRes.getArrayR());
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.err.println("Выход за пределы массива");
+        }
+
     }
 }
